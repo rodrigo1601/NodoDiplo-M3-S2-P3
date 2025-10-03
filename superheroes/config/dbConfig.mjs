a0fc0554e95cfab957591d6156437df6a736f6d7
rodrigo1601 <ribanez0116@gmail.com>
2025-10-03 05:50:48 -0300

    update: reemplazo por env

--- superheroes/config/dbConfig.mjs
+++ superheroes/config/dbConfig.mjs
@@ -1,8 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import mongoose from "mongoose";
 
 export async function connectDB(){
     try{
-        await mongoose.connect('mongodb+srv://Grupo-19:grupo19@cursadanodejs.ls9ii.mongodb.net/Node-js');
+        await mongoose.connect(process.env.MONGO_DB_PASSWORD);
         console.log('Conectado a MongoDB');
     }catch(error){
         console.error('error de conexión:', error);
